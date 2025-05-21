727b556299177e03e0ef4f10c1b31ca6663d0b5e
Jacob Maurer <jacob.maurer.24@cnu.edu>
2025-05-21 00:56:11 -0400

    added more unittests; tests all passed

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ def score_card(card: str, trump: str, starting_suite: str):
     trump_convert = {'9': 7, '0': 8, 'Q': 9, 'K': 10, 'A': 11}
     non_trump_convert = {'9': 1, '0': 2, 'J': 3, 'Q': 4, 'K': 5, 'A': 6}
     
-    #score the bauer (if it is a bauer)
+    #score Jack card seperate, since it has the possibillity of being big points
     if card[0] == 'J':
         bauer = get_bauer_type(card, trump)
         if bauer == 3:
@@ -11,9 +11,9 @@ def score_card(card: str, trump: str, starting_suite: str):
         elif bauer == 2:
             return 12
         elif bauer == 1:
-            return non_trump_convert[card[0]]
+            return non_trump_convert[card[0]] if card[1] == starting_suite else 0
     
-    #score any other card
+    #score any other card that is not a jack
     if not (card[1] == starting_suite or card[1] == trump):
         return 0 
     elif card[1] == trump:
--- tests/score_card_test.py
+++ tests/score_card_test.py
@@ -52,21 +52,100 @@ class TestScoreCard(unittest.TestCase):
         self.assertEqual(score_card('AH', 'S', 'H'), 6)
         
     def test_non_trump_diamond_start(self):
-        self.assertEqual(score_card('9D', 'C', 'D'), 1)
-        self.assertEqual(score_card('0D', 'C', 'D'), 2)
-        self.assertEqual(score_card('JD', 'C', 'D'), 3)
-        self.assertEqual(score_card('QD', 'C', 'D'), 4)
-        self.assertEqual(score_card('KD', 'C', 'D'), 5)
-        self.assertEqual(score_card('AD', 'C', 'D'), 6)
+        self.assertEqual(score_card('9D', 'S', 'D'), 1)
+        self.assertEqual(score_card('0D', 'S', 'D'), 2)
+        self.assertEqual(score_card('JD', 'S', 'D'), 3)
+        self.assertEqual(score_card('QD', 'S', 'D'), 4)
+        self.assertEqual(score_card('KD', 'S', 'D'), 5)
+        self.assertEqual(score_card('AD', 'S', 'D'), 6)
     
-    def test_non_trump_non_suite(self):
-        pass
+    def test_non_trump_non_suite_heart(self):
+        self.assertEqual(score_card('9H', 'S', 'C'), 0)
+        self.assertEqual(score_card('0H', 'S', 'C'), 0)
+        self.assertEqual(score_card('JH', 'S', 'C'), 0)
+        self.assertEqual(score_card('QH', 'S', 'C'), 0)
+        self.assertEqual(score_card('KH', 'S', 'C'), 0)
+        self.assertEqual(score_card('AH', 'S', 'C'), 0)
     
-    def test_trump_non_suite(self):
-        pass
+    def test_non_trump_non_suite_diamond(self):
+        self.assertEqual(score_card('9D', 'S', 'C'), 0)
+        self.assertEqual(score_card('0D', 'S', 'C'), 0)
+        self.assertEqual(score_card('JD', 'S', 'C'), 0)
+        self.assertEqual(score_card('QD', 'S', 'C'), 0)
+        self.assertEqual(score_card('KD', 'S', 'C'), 0)
+        self.assertEqual(score_card('AD', 'S', 'C'), 0)
     
-    def test_trump_suite(self):
-        pass
+    def test_non_trump_non_suite_club(self):
+        self.assertEqual(score_card('9C', 'H', 'D'), 0)
+        self.assertEqual(score_card('0C', 'H', 'D'), 0)
+        self.assertEqual(score_card('JC', 'H', 'D'), 0)
+        self.assertEqual(score_card('QC', 'H', 'D'), 0)
+        self.assertEqual(score_card('KC', 'H', 'D'), 0)
+        self.assertEqual(score_card('AC', 'H', 'D'), 0)
+    
+    def test_non_trump_non_suite_spade(self):
+        self.assertEqual(score_card('9S', 'H', 'D'), 0)
+        self.assertEqual(score_card('0S', 'H', 'D'), 0)
+        self.assertEqual(score_card('JS', 'H', 'D'), 0)
+        self.assertEqual(score_card('QS', 'H', 'D'), 0)
+        self.assertEqual(score_card('KS', 'H', 'D'), 0)
+        self.assertEqual(score_card('AS', 'H', 'D'), 0)
+    
+    def test_trump_heart_non_suite(self):
+        self.assertEqual(score_card('9H', 'H', 'D'), 7)
+        self.assertEqual(score_card('0H', 'H', 'D'), 8)
+        self.assertEqual(score_card('QH', 'H', 'D'), 9)
+        self.assertEqual(score_card('KH', 'H', 'D'), 10)
+        self.assertEqual(score_card('AH', 'H', 'D'), 11)
+    
+    def test_trump_diamond_non_suite(self):
+        self.assertEqual(score_card('9D', 'D', 'H'), 7)
+        self.assertEqual(score_card('0D', 'D', 'H'), 8)
+        self.assertEqual(score_card('QD', 'D', 'H'), 9)
+        self.assertEqual(score_card('KD', 'D', 'H'), 10)
+        self.assertEqual(score_card('AD', 'D', 'H'), 11)
+    
+    def test_trump_club_non_suite(self):
+        self.assertEqual(score_card('9C', 'C', 'S'), 7)
+        self.assertEqual(score_card('0C', 'C', 'S'), 8)
+        self.assertEqual(score_card('QC', 'C', 'S'), 9)
+        self.assertEqual(score_card('KC', 'C', 'S'), 10)
+        self.assertEqual(score_card('AC', 'C', 'S'), 11)
+    
+    def test_trump_spade_non_suite(self):
+        self.assertEqual(score_card('9S', 'S', 'C'), 7)
+        self.assertEqual(score_card('0S', 'S', 'C'), 8)
+        self.assertEqual(score_card('QS', 'S', 'C'), 9)
+        self.assertEqual(score_card('KS', 'S', 'C'), 10)
+        self.assertEqual(score_card('AS', 'S', 'C'), 11)
+    
+    def test_trump_diamond_suite(self):
+        self.assertEqual(score_card('9D', 'D', 'D'), 7)
+        self.assertEqual(score_card('0D', 'D', 'D'), 8)
+        self.assertEqual(score_card('QD', 'D', 'D'), 9)
+        self.assertEqual(score_card('KD', 'D', 'D'), 10)
+        self.assertEqual(score_card('AD', 'D', 'D'), 11)
+    
+    def test_trump_heart_suite(self):
+        self.assertEqual(score_card('9H', 'H', 'H'), 7)
+        self.assertEqual(score_card('0H', 'H', 'H'), 8)
+        self.assertEqual(score_card('QH', 'H', 'H'), 9)
+        self.assertEqual(score_card('KH', 'H', 'H'), 10)
+        self.assertEqual(score_card('AH', 'H', 'H'), 11)
+    
+    def test_trump_club_suite(self):
+        self.assertEqual(score_card('9C', 'C', 'C'), 7)
+        self.assertEqual(score_card('0C', 'C', 'C'), 8)
+        self.assertEqual(score_card('QC', 'C', 'C'), 9)
+        self.assertEqual(score_card('KC', 'C', 'C'), 10)
+        self.assertEqual(score_card('AC', 'C', 'C'), 11)
+        
+    def test_trump_spade_suite(self):
+        self.assertEqual(score_card('9S', 'S', 'S'), 7)
+        self.assertEqual(score_card('0S', 'S', 'S'), 8)
+        self.assertEqual(score_card('QS', 'S', 'S'), 9)
+        self.assertEqual(score_card('KS', 'S', 'S'), 10)
+        self.assertEqual(score_card('AS', 'S', 'S'), 11)
         
 if __name__ == '__main__':
     unittest.main()
